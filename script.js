2fc440796f4fca267c35ad882a696c279334c621
Tommy Ju <tommyju25@gmail.com>
2023-05-03 20:44:25 -0700

    Implement form validation and pseudo classes

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const body = document.querySelector('body');
 const formControls = document.querySelector(".form-controls");
+const pass = document.querySelector("#pass");
+const confirmPass = document.querySelector('#confirm-pass');
+const form = document.querySelector('.basic-form');
+const legend = document.querySelector("#legend");
 
 // Changes flex direction of form controls when screen height > width
 window.addEventListener('resize', () => {
@@ -10,3 +14,23 @@ window.addEventListener('resize', () => {
         formControls.style.cssText = 'flex-direction: row';
     }
 })
+
+function validateForm() {
+    if (pass.value === confirmPass.value) {
+        legend.textContent = "Account created successfully!";
+        pass.classList.remove("error");
+        confirmPass.classList.remove("error");
+        
+    }
+    else {
+        legend.textContent = "Passwords do not match";
+        pass.classList.add("error");
+        confirmPass.classList.add("error");
+    }
+
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validateForm();
+});
